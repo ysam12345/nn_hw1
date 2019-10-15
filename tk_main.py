b21c13cc0e59ab9ce2278302de3a512adfa048b2
游子謙 <yochien@youziqiande-MBP.local>
2019-10-15 17:14:28 +0800

    add figure of training data

--- tk_main.py
+++ tk_main.py
@@ -47,10 +47,15 @@ class Application(tk.Frame):
 
 
         # 設定訓練圖表
-        self.training_figure = Figure(figsize=(3,3), dpi=100)
-        self.training_canvas = FigureCanvasTkAgg(self.training_figure, self)
-        self.training_canvas.draw()
-        self.training_canvas.get_tk_widget().grid(row=3, columnspan=3)
+        self.training_acc_figure = Figure(figsize=(3,3), dpi=100)
+        self.training_acc_canvas = FigureCanvasTkAgg(self.training_acc_figure, self)
+        self.training_acc_canvas.draw()
+        self.training_acc_canvas.get_tk_widget().grid(row=3, column=0, columnspan=3)
+
+        self.training_data_figure = Figure(figsize=(3,3), dpi=100)
+        self.training_data_canvas = FigureCanvasTkAgg(self.training_data_figure, self)
+        self.training_data_canvas.draw()
+        self.training_data_canvas.get_tk_widget().grid(row=3, column=4, columnspan=3)
 
         # 相關結果文字
         self.training_acc_label = tk.Label(self)
@@ -75,24 +80,44 @@ class Application(tk.Frame):
 
         self.weight_text = tk.Text(self)
         self.weight_text["height"] = 10
-        self.weight_text["width"] = 30
+        self.weight_text["width"] = 40
         self.weight_text.grid(row=6, column=1, sticky=tk.N+tk.W)
 
-    def draw_training_figure(self, train_result_list):
+
+
+    def draw_training_acc_figure(self, train_result_list):
         #清空影像
-        self.training_figure.clf()
-        self.training_figure.a = self.training_figure.add_subplot(111)
+        self.training_acc_figure.clf()
+        self.training_acc_figure.a = self.training_acc_figure.add_subplot(111)
         acc_list = [i['acc'] for i in train_result_list]
     
         #繪製正確率折線圖
-        self.training_figure.a.plot(acc_list)
-        self.training_figure.a.set_title('Traing: Acc/Epoch')
-        self.training_canvas.draw()
+        self.training_acc_figure.a.plot(acc_list)
+        self.training_acc_figure.a.set_title('Traing: Acc/Epoch')
+        self.training_acc_canvas.draw()
+
+
+    def draw_training_data_figure(self, dataset):
+        #清空影像
+        self.training_data_figure.clf()
+        self.training_data_figure.a = self.training_data_figure.add_subplot(111)
+        X = dataset[0].values.reshape(-1,).tolist()
+        y = dataset[1].values.reshape(-1,).tolist()
+        print(dataset)
+        print(X)
+        print(y)
+        #繪製正確率折線圖
+        self.training_data_figure.a.plot(X, y, 'ro')
+        self.training_data_figure.a.set_title('Traing Data')
+        self.training_data_canvas.draw()
+
 
         
     def select_file_and_run(self):
         filename = askopenfilename()
         df = pd.read_table(filename, sep=" ", header=None)
+
+        self.draw_training_data_figure(df)
 
         # split traning data and testing data
         train_df=df.sample(frac=0.666666)
@@ -122,7 +147,7 @@ class Application(tk.Frame):
             if train_result["acc"] > int(self.early_stop_spinbox.get()):
                 break
         print("### training end ###")
-        self.draw_training_figure(train_result_list)
+        self.draw_training_acc_figure(train_result_list)
         self.training_acc_text_label["text"] = train_result_list[len(train_result_list)-1]["acc"]
         self.weight_text.delete(1.0, END) 
         self.weight_text.insert(1.0, train_result_list[len(train_result_list)-1]["weight"]) 
